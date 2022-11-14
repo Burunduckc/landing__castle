803bb3aae6d5483208c7006941d7c601f992df31
Burunduckc <1999Melkymov@gmail.com>
2022-11-14 11:59:00 +0300

    more alc :)

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import './index.html';
+import './sass/style.scss';
+import alc from'./img/alchohol.png';
+import alcSmall from'./img/alcohol__small.png'
+import alcVerySmall from'./img/alcohol__very_small.png'
+import descr1 from'./img/descr1.png';
+import descr2 from'./img/descr2.png';
+import descr3 from'./img/descr3.png';
+import descr4 from'./img/descr4.png';
+import footMan from'./img/footer_man.png';
+import footManBig from'./img/footer_man_big.png';
+import fotManSmall from'./img/footer_man_small.png';
+import group from './img/group.png';
+import stol from './img/img__stol.png';
+import small from  './img/img_small.png';
+import woomenVerySmall from './img/img_woomen_very__small.png';
+import woomen from './img/img_woomen.png';
+import img from './img/img.png';
+import logo from './img/logo.png';
+import placeholder from './img/placeholder.png';
+import smartphone from './img/smart.png';
+import video from './img/video.png';
